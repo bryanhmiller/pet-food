2c134a6be5f16a3a3f6e4e8b4fd3928d804272c8
bryanhmiller <bryankalos@hotmail.com>
2017-03-08 13:31:41 -0600

    Finished exercise, completed catFood.js, corrected more capitalizations in dogFood.js, finished all of the functions in food.js to populate innerHTML, added a div for catfood cards in index.html

--- food.js
+++ food.js
@@ -1,6 +1,7 @@
 // Your first task is to build a product page that displays all brands of dog food, 
 // the different types, with the price and size for each container volume.
-var foodBowl = document.getElementById("foodContainer");
+var dogFoodBowl = document.getElementById("dogFoodContainer");
+var catFoodBowl = document.getElementById("catFoodContainer");
 
 function makeDogFoodDom(xhrData){
 	var foodString = "";
@@ -14,53 +15,69 @@ function makeDogFoodDom(xhrData){
 		foodString += `<h4>Brand:</h4>`;
 		foodString += `<h4>${currentFood.name} Dog Food</h4>`;
 		foodString += `<p>Types:</p>`;
-		for (var q = 0; q < currentFood.types.length; q++) {
-			foodString += `<p>${currentFood.types[q].type}</p>`;
-			console.log(currentFood.types[q].type);
-			console.log(currentFood.types[q].volumes);
-			for (var j = 0; j < currentFood.types[q].volumes.length; j++) {
-				foodString += `<p>Size: ${currentFood.types[q].volumes[j].name}`;
-				foodString += `<p>$${currentFood.types[q].volumes[j].price}`;
+		for (var j = 0; j < currentFood.types.length; j++) {
+			foodString += `<p>${currentFood.types[j].type}</p>`;
+			console.log(currentFood.types[j].type);
+			console.log(currentFood.types[j].volumes);
+			for (var k = 0; k < currentFood.types[j].volumes.length; k++) {
+				foodString += `<p>Size: ${currentFood.types[j].volumes[k].name}`;
+				foodString += `<p>$${currentFood.types[j].volumes[k].price}`;
 			}
 		}
 		foodString += `</div></div></div>`;
 	}	
-	// for (var k = 0; k < xhrData.cat_brands.length; k++) {
-	// 	currentFood = xhrData.cat_brands[k];
-
-	// 	foodString += `<div class="col-sm-6 col-md-4">`;
-	// 	foodString += `<div class="thumbnail">`;
-	// 	foodString += `<div class="caption">`;
-	// 	foodString += `<h4>Brand:</h4>`;
-	// 	foodString += `<h4>${currentFood.name} Dog Food</h4>`;
-	// 	foodString += `<p>Types:</p>`;
-	// 	for (var l = 0; l < currentFood.types.length; l++) {
-	// 		foodString += `<p>${currentFood.types[l].type}</p>`;
-	// 		console.log(currentFood.types[l].type);
-	// 		console.log(currentFood.types[l].volumes);
-	// 		for (var m = 0; m < currentFood.types[l].volumes.length; m++) {
-	// 			foodString += `<p>Size: ${currentFood.types[l].volumes[m].name}`;
-	// 			foodString += `<p>$${currentFood.types[l].volumes[m].price}`;
-	// 		}
-	// 	}
-	// 	foodString += `</div></div></div>`;
-	// }	
-	foodBowl.innerHTML = foodString;
+	dogFoodBowl.innerHTML = foodString;
 }
 
+function makeCatFoodDom(xhrData){
+	var foodString = "";
+	var currentFood;
+	for (var l = 0; l < xhrData.cat_brands.length; l++) {
+		currentFood = xhrData.cat_brands[l];
 
-
-function executeThisCodeAfterFileLoaded(){
-	var data = JSON.parse(this.responseText);
-	makeDogFoodDom(data);
+		foodString += `<div class="col-sm-6 col-md-4">`;
+		foodString += `<div class="thumbnail">`;
+		foodString += `<div class="caption">`;
+		foodString += `<h4>Brand:</h4>`;
+		foodString += `<h4>${currentFood.name} Cat Food</h4>`;
+		foodString += `<h5>Breeds: ${currentFood.breeds}</h5>`;
+		foodString += `<p>Types:</p>`;
+		for (var m = 0; m < currentFood.types.length; m++) {
+			foodString += `<p>${currentFood.types[m].type}</p>`;
+			console.log(currentFood.types[m].type);
+			console.log(currentFood.types[m].volumes);
+			for (var n = 0; n < currentFood.types[m].volumes.length; n++) {
+				foodString += `<p>Size: ${currentFood.types[m].volumes[n].name}`;
+				foodString += `<p>$${currentFood.types[m].volumes[n].price}`;
+			}
+		}
+		foodString += `</div></div></div>`;
+	}	
+	catFoodBowl.innerHTML = foodString;
 }
 
-function executeThisCodeAfterFileFails(){
+function dogFoodMakerLoaded(){
+	var dogData = JSON.parse(this.responseText);
+	makeDogFoodDom(dogData);
+}
+
+function catFoodMakerLoaded(){
+	var catData = JSON.parse(this.responseText);
+	makeCatFoodDom(catData);
+}
+
+function petFoodFail(){
 	console.log("booooo");
 }
 
 var dogFoodRequest = new XMLHttpRequest();
-dogFoodRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-dogFoodRequest.addEventListener("error", executeThisCodeAfterFileFails);
+dogFoodRequest.addEventListener("load", dogFoodMakerLoaded);
+dogFoodRequest.addEventListener("error", petFoodFail);
 dogFoodRequest.open("GET", "dogFood.json");
 dogFoodRequest.send();
+
+var catFoodRequest = new XMLHttpRequest();
+catFoodRequest.addEventListener("load", catFoodMakerLoaded);
+catFoodRequest.addEventListener("error", petFoodFail);
+catFoodRequest.open("GET", "catFood.json");
+catFoodRequest.send();
