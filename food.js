bd9e53be280c464aa31d957be71b5680924ab933
bryanhmiller <bryankalos@hotmail.com>
2017-03-08 12:48:34 -0600

    Started catFood.json added some code to food.js for catFood, made a capitalization correction to dogFood.json

--- food.js
+++ food.js
@@ -2,17 +2,17 @@
 // the different types, with the price and size for each container volume.
 var foodBowl = document.getElementById("foodContainer");
 
-function makeDom(xhrData){
+function makeDogFoodDom(xhrData){
 	var foodString = "";
 	var currentFood;
-	for (var i = 0; i < xhrData.dog_brands.length; i ++) {
+	for (var i = 0; i < xhrData.dog_brands.length; i++) {
 		currentFood = xhrData.dog_brands[i];
 
 		foodString += `<div class="col-sm-6 col-md-4">`;
 		foodString += `<div class="thumbnail">`;
 		foodString += `<div class="caption">`;
 		foodString += `<h4>Brand:</h4>`;
-		foodString += `<h3>${currentFood.name}</h3>`;
+		foodString += `<h4>${currentFood.name} Dog Food</h4>`;
 		foodString += `<p>Types:</p>`;
 		for (var q = 0; q < currentFood.types.length; q++) {
 			foodString += `<p>${currentFood.types[q].type}</p>`;
@@ -23,10 +23,28 @@ function makeDom(xhrData){
 				foodString += `<p>$${currentFood.types[q].volumes[j].price}`;
 			}
 		}
-
 		foodString += `</div></div></div>`;
 	}	
+	// for (var k = 0; k < xhrData.cat_brands.length; k++) {
+	// 	currentFood = xhrData.cat_brands[k];
 
+	// 	foodString += `<div class="col-sm-6 col-md-4">`;
+	// 	foodString += `<div class="thumbnail">`;
+	// 	foodString += `<div class="caption">`;
+	// 	foodString += `<h4>Brand:</h4>`;
+	// 	foodString += `<h4>${currentFood.name} Dog Food</h4>`;
+	// 	foodString += `<p>Types:</p>`;
+	// 	for (var l = 0; l < currentFood.types.length; l++) {
+	// 		foodString += `<p>${currentFood.types[l].type}</p>`;
+	// 		console.log(currentFood.types[l].type);
+	// 		console.log(currentFood.types[l].volumes);
+	// 		for (var m = 0; m < currentFood.types[l].volumes.length; m++) {
+	// 			foodString += `<p>Size: ${currentFood.types[l].volumes[m].name}`;
+	// 			foodString += `<p>$${currentFood.types[l].volumes[m].price}`;
+	// 		}
+	// 	}
+	// 	foodString += `</div></div></div>`;
+	// }	
 	foodBowl.innerHTML = foodString;
 }
 
@@ -34,15 +52,15 @@ function makeDom(xhrData){
 
 function executeThisCodeAfterFileLoaded(){
 	var data = JSON.parse(this.responseText);
-	makeDom(data);
+	makeDogFoodDom(data);
 }
 
 function executeThisCodeAfterFileFails(){
 	console.log("booooo");
 }
 
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-myRequest.addEventListener("error", executeThisCodeAfterFileFails);
-myRequest.open("GET", "dogFood.json");
-myRequest.send();
+var dogFoodRequest = new XMLHttpRequest();
+dogFoodRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+dogFoodRequest.addEventListener("error", executeThisCodeAfterFileFails);
+dogFoodRequest.open("GET", "dogFood.json");
+dogFoodRequest.send();
